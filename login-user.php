37043b2acedd39fe043630a17b3b53eea07ff098
Abdulwahab Nasir <weybansky@gmail.com>
2019-09-17 10:31:47 +0100

    implemented remember me

--- login-user.php
+++ login-user.php
@@ -2,8 +2,31 @@
 // start session
 session_start();
 
+// if logged in already, redirect 
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true) {
     header("location:welcome.php");
+}
+
+// if remeber was set before, login directly
+if (isset($_COOKIE["heistuser"])) {
+    if (file_exists('database/users.json')) {
+        $users = json_decode(file_get_contents("database/users.json"));
+        $usernames = array_column($users, "username");
+        if (in_array($_COOKIE["heistuser"], $usernames)) {
+            $user = $users[array_search($_COOKIE["heistuser"], $usernames)];
+                // store all vars in session
+                $_SESSION['loggedin'] = true;
+                $_SESSION['fullname'] = $user->fullname;
+                $_SESSION['username'] = $user->username;
+                $_SESSION['email'] = $user->email;
+                $_SESSION['phone'] = $user->phone;
+                header("location:welcome.php");
+        } else {
+            echo "User does not exist";
+        }
+    } else {
+        echo "Database not present";
+    }
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
@@ -23,7 +46,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
             if (in_array($username, $usernames)) {
                 $user = $users[array_search($username, $usernames)];
 
-                if ($password == $user->password) {
+                if (md5($password) == $user->password) {
+                    // if remember me isset
+                    if (isset($_POST["remember_me"])) {
+                        setcookie("heistuser", $_POST["username"], time() + (30 * 24 * 60 * 60));
+                    } else {
+                        if (isset($_COOKIE["heistuser"])) {
+                            setcookie("heistuser", "");
+                        }
+                    }
+                    
                     // store all vars in session
                     $_SESSION['loggedin'] = true;
                     $_SESSION['fullname'] = $user->fullname;
